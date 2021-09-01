4631061537d0f34f726ef994bc89a225a7854b33
Genkai <75561611+Shreyansh-Rai@users.noreply.github.com>
2021-09-02 00:44:38 +0530

    test3

--- public/script.js
+++ public/script.js
@@ -129,8 +129,8 @@ socket.on('ul',(un,pidl)=>
 
     //console.log(pidl)
     
-    var vfe = document.getElementById(`${pidl}`)
-    vfe.remove()
+    //var vfe = document.getElementById(`${pidl}`)
+    //vfe.remove()
     const newtext = document.createElement('div')
     newtext.innerText=`${un} has left`
     newtext.classList.add('message')
